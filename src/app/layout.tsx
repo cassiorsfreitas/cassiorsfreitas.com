8f48eb994ef659a8889556f1e822a3f8b5f9a7b7
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2023-12-20 21:09:29 +0000

    feat: add blog, stack and work routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Fira_Code } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Fira_Code({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',

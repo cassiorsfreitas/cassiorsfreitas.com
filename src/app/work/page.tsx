11ce97eb39f3b9d25ab66debe046519794931522
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2023-12-21 22:17:22 +0000

    feat: add work history

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -4,6 +4,66 @@ export default function Work() {
       <h1 className="font-medium text-2xl mb-8 tracking-tighter text-transparent bg-clip-text bg-gradient-to-br from-gradient-purple to-gradient-cyan">
         .check my work
       </h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Dedicated to shaping products that resonate with developers while
+          nurturing the growth of the next-gen tech enthusiasts. Let me share a
+          glimpse of my journey thus far.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Metyis</h2>
+        <p className="text-grey">Software Engineer</p>
+        <p className="text-grey">
+          <a href="https://www.hugoboss.com/">HUGO BOSS (HBDC) - 2023/Today</a>{' '}
+          - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis
+          vel massa et cursus. Curabitur placerat, quam vitae vulputate tempor,
+          tellus turpis ultrices metus, eget venenatis elit metus at erat.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Agap2IT</h2>
+        <p className="text-grey">Software Developer</p>
+        <p className="text-grey mb-4">
+          <a href="https://www.calvinklein.com/">
+            TOMMY HILFIGER & CALVIN KLEIN (NETHERLANDS) - 2022/2023
+          </a>{' '}
+          - I&apos;m currently working on the frontend using ReactJS and Redux.
+          My tasks include creating and optimizing interfaces, unit tests, and
+          storybooks. Additionally, I connect the components with GraphQL API
+          services to ensure seamless communication. On the backend, my focus
+          lies in developing and improving the API using NodeJS, aiming to
+          enhance its performance and functionality.
+        </p>
+        <p className="text-grey mb-4">
+          <a href="https://ambisig.com/">AMBISIG (PORTUGAL) - 2021/2022</a> - I
+          worked on developing React components and integrating the Rest API of
+          the Portuguese Government&apos;s Traffic Offense System with external
+          services such as Google API. My objective was to develop scalable and
+          testable code from scratch.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+          Blend Euro
+        </h2>
+        <p className="text-grey">Full Stack Developer</p>
+        <p className="text-grey">
+          <a href="https://www.ype.ind.br/">YPÊ (PORTUGAL) - 2021</a> - I worked
+          on developing NextJs (ReactJS) projects on the frontend and Express
+          (NodeJS) on the backend, integrating them with AWS and Salesforce
+          services.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Musique</h2>
+        <p className="text-grey">Full Stack Developer</p>
+        <p className="text-grey">
+          <a href="https://www.projetomusique.com.br/">
+            Freelancer (BRAZIL) - 2019/2020
+          </a>{' '}
+          - As a software developer, I worked on the creation and presentation
+          of the company&apos;s main website, the help center website, and the
+          management of search engine optimizations (SEO) using HTML, CSS, JS,
+          and WordPress as a headless CMS.
+        </p>
+      </div>
     </section>
   )
 }

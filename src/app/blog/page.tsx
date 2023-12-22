9f0abacbac883b97c39f30cb4b694cd1e59d3880
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2023-12-22 15:29:26 +0000

    feat: add stack content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -14,6 +14,13 @@ export default function BlogPage() {
       <h1 className="font-medium text-2xl mb-8 tracking-tighter text-transparent bg-clip-text bg-gradient-to-br from-gradient-cyan to-gradient-green">
         .read my blog
       </h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Browse through my articles on web development, software engineering,
+          and navigating tech careers, available in English and Portuguese.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      </div>
       {allBlogs
         .sort((a, b) => {
           if (
--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -4,6 +4,56 @@ export default function Work() {
       <h1 className="font-medium text-2xl mb-8 tracking-tighter text-transparent bg-clip-text bg-gradient-to-br from-gradient-pink to-gradient-purple">
         .get my stuff
       </h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          This document stays dynamic, always evolving with my current tools and
+          recommendations (many of which are free). Feel at liberty to sample a
+          bit of everything here.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Keyboard</h2>
+        <p className="text-grey">
+          - Split Corne MX 42 Switches KTT Kang crafted by{' '}
+          <a href="https://twitter.com/neversaytop">@neversaytop</a>
+          <br /> - <a href="https://nuphy.com/">NuPhy</a> Air75 V2 Cowberry
+          (45gf)
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Desk</h2>
+        <p className="text-grey">
+          - Laptop <a href="https://www.apple.com/">MacBook</a> Pro M1 16GB 1TB
+          <br /> - Monitor <a href="https://www.asus.com/">Asus</a> VG248QG (24
+          - 165Hz)
+          <br /> - Webcam <a href="https://www.logitech.com/">Logitech</a> C920s
+          Pro
+          <br /> - Mouse <a href="https://www.logitech.com/">Logitech</a> MX
+          Master 3
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Coding</h2>
+        <p className="text-grey">
+          - Neovim with <a href="https://astronvim.com/">AstroNvim</a>
+          <br /> - VSCode with{' '}
+          <a href="https://github.com/cassiorsfreitas/vscodevim-configs">
+            Vim configs
+          </a>
+          <br /> - iTerm2 with{' '}
+          <a href="https://github.com/tmux/tmux/wiki">Tmux</a>
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Apps</h2>
+        <p className="text-grey">
+          - <a href="https://www.notion.so/">Notion</a> for Notes{' '}
+          <del>and everything</del>
+          <br /> - <a href="https://bitwarden.com/">BitWarden</a> Password
+          Manager
+          <br /> - <a href="https://cron.com/">Cron</a> Calendar
+          <br /> - <a href="https://arc.net/">Arc</a> Browser
+          <br /> - <a href="https://figma.com/">Figma</a> for Design
+          <br /> - <a href="https://www.raycast.com/">Raycast</a> as Spotlight
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      </div>
     </section>
   )
 }

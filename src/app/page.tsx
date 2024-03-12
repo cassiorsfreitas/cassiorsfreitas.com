16288d587c441a6a19b928d07b898b29ddd5aa32
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2024-03-12 19:06:06 +0000

    feat: add hugoboss and metyis in the home description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,25 @@ export default function Home() {
       <h1 className="text-4xl lg:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-gradient-yellow to-gradient-pink">
         hey, I&apos;m <br /> Cássio Freitas
       </h1>
-      <h2 className="text-lg lg:text-2xl text-grey mt-2">
-        Software Engineer at Metyis
+      <h2 className="text-lg lg:text-lg text-grey mt-2">
+        Software Engineer at{' '}
+        <a
+          className="text-gray-600"
+          target="_blank"
+          href="https://www.hugoboss.com/"
+          rel="noreferrer"
+        >
+          @HugoBoss
+        </a>{' '}
+        through{' '}
+        <a
+          className="text-gray-600"
+          target="_blank"
+          href="https://www.metyis.com/"
+          rel="noreferrer"
+        >
+          @Metyis
+        </a>
       </h2>
     </main>
   )
